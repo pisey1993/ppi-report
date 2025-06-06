9e7640e6e7ce1f5830829056dfaa9ac09db38cb4
pisey1993 <pisey@peoplenpartners.com>
2025-06-06 08:42:36 +0700

    update app

--- app/Views/layout/nav.php
+++ app/Views/layout/nav.php
@@ -13,7 +13,7 @@
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
                     <li class="nav-item">
-                        <a class="nav-link" href="/"> Dashboard</a>
+                        <a class="nav-link" href="https://peoplenpartners.com/public/ppi-report/"> Dashboard</a>
                     </li> <li class="nav-item">
                         <a class="nav-link" href="quote-report"> Quote Listing</a>
                     </li> <li class="nav-item">
